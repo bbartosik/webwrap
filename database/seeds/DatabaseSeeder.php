48f5c3637f7d71373eb729085b703574b4b1129a
Bart Bartosik <bbartosik@g.harvard.edu>
2015-12-21 12:26:29 -0500

    seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        //  $this->call(Website_Table_Seeder::class);
-        //  $this->call(LogosTableSeeder::class);
+          $this->call(Website_Table_Seeder::class);
+          $this->call(LogosTableSeeder::class);
           $this->call(UsersTableSeeder::class);
 
         Model::reguard();
